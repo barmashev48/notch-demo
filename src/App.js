24e10fc6155887433b7102a2b350bfe89ef7a4f0
Ermek Barmashev <barmashev48@gmail.com>
2021-10-25 14:46:42 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -10,34 +10,12 @@ import DATA from "./DATA";
 
 function App() {
   const dispatch = useDispatch();
-  const [allItems, setAllItems] = useState(DATA);
-  const [filteredItems, setFilteredItems] = useState(allItems);
-
-  const [categoryFilters, setCategoryFilters] = useState([]);
 
   const [isFilterDisplayed, setIsFilterDisplayed] = useState(true);
   const [isCartDisplayed, setIsCartDisplayed] = useState(false);
 
   //dynamically set the category filters
   useEffect(() => {
-    // const allCategories = allItems.map((item) => {
-    //   return { categoryName: item.category, selected: false };
-    // });
-    // const noDuplicateCategories = allCategories.filter(
-    //   (item, index, arr) =>
-    //     index ===
-    //     arr.findIndex(
-    //       (el) =>
-    //         el.categoryName === item.categoryName &&
-    //         el.selected === item.selected
-    //     )
-    // );
-    // const filtersToDisplay = [
-    //   { categoryName: "Show All", selected: true },
-    //   ...noDuplicateCategories,
-    // ];
-    // setCategoryFilters(filtersToDisplay);
-    // dispatch(filteredItemsActions.setFilters());
     dispatch(filteredItemsActions.setFilters());
   }, []);
 
@@ -47,31 +25,6 @@ function App() {
 
   const onToggleCart = () => {
     setIsCartDisplayed((prevState) => !prevState);
-  };
-
-  const onFilterByCategory = (category) => {
-    if (category === "Show All") {
-      setFilteredItems(allItems);
-      const newFilters = categoryFilters.map((item) => {
-        if (item.categoryName === "Show All") {
-          return { categoryName: item.categoryName, selected: true };
-        }
-        return { categoryName: item.categoryName, selected: false };
-      });
-      setCategoryFilters(newFilters);
-      return;
-    }
-    const newFilters = categoryFilters.map((item) => {
-      if (category === item.categoryName) {
-        return { categoryName: item.categoryName, selected: true };
-      }
-      return { categoryName: item.categoryName, selected: false };
-    });
-    setCategoryFilters(newFilters);
-    const newFilteredItems = allItems.filter((item) => {
-      return item.category === category;
-    });
-    setFilteredItems(newFilteredItems);
   };
 
   return (
@@ -84,15 +37,11 @@ function App() {
         />
         <main className={styles.main}>
           {isFilterDisplayed && (
-            <FilterSection
-              categories={categoryFilters}
-              onFilter={onFilterByCategory}
-              onToggleFilters={onToggleFilters}
-            />
+            <FilterSection onToggleFilters={onToggleFilters} />
           )}
 
           {isCartDisplayed && <Cart onToggleCart={onToggleCart} />}
-          <MarketSection items={filteredItems} />
+          <MarketSection />
         </main>
       </div>
     </div>
